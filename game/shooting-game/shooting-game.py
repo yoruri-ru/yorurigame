329258120e93daf91c89c3599d4cbfe909829142
yoruri-ru <kakentuichan@gmail.com>
2024-10-23 23:50:51 +0900

    Update shooting-game.py

--- game/shooting-game/shooting-game.py
+++ game/shooting-game/shooting-game.py
@@ -1,24 +1,26 @@
 import pyxel
 
-class App:
-    def __init__(self):
-        pyxel.init(100, 100)
+pyxel.init(160,128)
+pyxel.load("shooting-game.pyxres")
 
-        # リソースファイルの読み込み
-        pyxel.load("shooting-game.pyxres")
-        
-    def run(self):
-        pyxel.run(self.update, self.draw)
+x = 0
+y = 50
+def update():
+    global x,y,dx
+    if pyxel.btn(pyxel.KEY_LEFT):
+        dx = -2
+    elif pyxel.btn(pyxel.KEY_RIGHT):
+        dx = 2
+    else:
+        dx = 0
 
-    def update(self):
-        pass
+    # 横方向の移動
+    x = x + dx
+    return
 
-    def draw(self):
-        pyxel.cls(0)
+def draw():
+    pyxel.cls(1)
+    pyxel.blt( x,y, 0, 0,0, 16,16, 0)
+    return
 
-        # イメージの描画：(x, y, img, u, v, w, h, [colkey])
-        # xy:コピー先の座標、img:イメージバンクの番号
-        # uv:コピー元の座標、wh:コピー範囲、colkey:透明色
-        pyxel.blt(0, 0, 0, 0, 0, 16, 16, 0)
-
-App().run()
+pyxel.run(update,draw)
