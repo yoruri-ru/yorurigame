0ddef0cd67cffac0c96e8610f47c148888ec3908
yoruri-ru <kakentuichan@gmail.com>
2024-12-11 10:39:13 +0900

    Update zyanken.py

--- game/shooting-game/zyanken.py
+++ game/shooting-game/zyanken.py
@@ -3,8 +3,9 @@ import pyxel
 class App:
     
     def __init__(self):
-        pyxel.init(240, 160, title="Pyxel Shooter r")
+        pyxel.init(80, 64, title="Pyxel Shooter r")
         pyxel.load("zyanken.pyxres")
+        pyxel.mouse(True)
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -12,6 +13,17 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
+
+        #COM
+        pyxel.text(4,10, "COM",7)
+        pyxel.blt(32,10, 0, 0,0, 16,16,0)
+
+        #PLAYER
+        pyxel.text(4,32, "YOU",7)
+        pyxel.blt(16,32, 0,  0,0, 16,16, 0)
+        pyxel.blt(32,32, 0, 16,0, 16,16, 0)
+        pyxel.blt(48,32, 0, 32,0, 16,16, 0)
+        
         return
 
 App()
