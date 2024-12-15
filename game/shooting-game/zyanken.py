9bc1432ac6154caa011ab83cb53fbc40a186b0ad
yoruri-ru <kakentuichan@gmail.com>
2024-12-15 14:47:36 +0900

    Update zyanken.py

--- game/shooting-game/zyanken.py
+++ game/shooting-game/zyanken.py
@@ -9,21 +9,15 @@ class App:
     def __init__(self):
         pyxel.init(80, 64, title="zyanken", fps=25)
         pyxel.load("zyanken.pyxres")
+        pyxel.mouse(True)
         pyxel.run(self.update, self.draw)
+
+    def check():
+        
 
     def update(self):
         global COM_HAND, PLAYER_HAND
-        if pyxel.btn(pyxel.KEY_S):
-            PLAYER_HAND = 0
-        if pyxel.btn(pyxel.KEY_A):
-            PLAYER_HAND = 1
-        if pyxel.btn(pyxel.KEY_D):
-            PLAYER_HAND = 2
-        if pyxel.btnp(pyxel.KEY_SPACE):
-            COM_HAND = random.randint(0,2)
-        if pyxel.btn(pyxel.KEY_W):
-            pyxel.playm(0, loop=True)
-        return
+       
         
     def draw(self):
         pyxel.cls(0)
@@ -34,8 +28,9 @@ class App:
 
         #PLAYER
         pyxel.text(4,32, "YOU",7)
-        pyxel.blt(32,32, 0,  PLAYER_HAND * 16,0, 16,16, 0)
-        
+        pyxel.blt(32,32, 0, 0,0, 16,16, 0)
+        pyxel.blt(32,32, 0, 16,0, 16,16, 0)
+        pyxel.blt(32,32, 0, 32,0, 16,16, 0)
         return
 
 App()
