aa5c22938ddeb81eee4bda7467d3e0b3915cd3d6
yoruri-ru <kakentuichan@gmail.com>
2024-12-16 08:05:28 +0000

    [update]

--- game/sumabura/sumabura.py
+++ game/sumabura/sumabura.py
@@ -1,1 +1,19 @@
+import pyxel
 
+class App:
+
+    def __init__(self):
+        pyxel.init(80, 64, title="スマブラ", fps="32")
+        pyxel.load("sumabura.pyxres")
+        pyxel.run(self.update, self.draw)
+    
+    def update(self):
+        return
+
+    def draw(self):
+        pyxel.cls(0)
+
+        pyxel.blt(32,32, 0, 16,0, 32,16, 0)
+
+
+App()
